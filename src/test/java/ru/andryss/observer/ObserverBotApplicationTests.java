5cc5e1b7586bde5b801d654cb6817bffd1542de2
Andryss <andr.krivosheev@gmail.com>
2025-06-12 19:46:16 +0300

    Init

---
+++ src/test/java/ru/andryss/observer/ObserverBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andryss.observer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
