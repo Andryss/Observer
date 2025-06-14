88787b1f7181d214a278433df928d6816ccc6e45
Andryss <andr.krivosheev@gmail.com>
2025-06-14 21:37:34 +0300

    Add yandexgpt api

---
+++ src/main/java/ru/andryss/observer/config/YandexGptConfiguration.java
@@ -0,0 +1,19 @@
+package ru.andryss.observer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import ru.andryss.observer.generated.yandexgpt.api.YandexGptApi;
+import ru.andryss.observer.generated.yandexgpt.invoker.ApiClient;
+
+@Configuration
+@Profile("!functionalTest")
+public class YandexGptConfiguration {
+
+    @Bean
+    public YandexGptApi yandexGptApi(YandexGptProperties properties) {
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(properties.getIamToken());
+        return new YandexGptApi(apiClient);
+    }
+}
---
+++ src/test/java/ru/andryss/observer/manual/ManualYandexGptApiTest.java
@@ -0,0 +1,58 @@
+package ru.andryss.observer.manual;
+
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import ru.andryss.observer.BaseDbTest;
+import ru.andryss.observer.generated.yandexgpt.api.YandexGptApi;
+import ru.andryss.observer.generated.yandexgpt.model.CompletionOptions;
+import ru.andryss.observer.generated.yandexgpt.model.CompletionRequest;
+import ru.andryss.observer.generated.yandexgpt.model.CompletionResponse;
+import ru.andryss.observer.generated.yandexgpt.model.Message;
+import ru.andryss.observer.generated.yandexgpt.model.MessageRole;
+import ru.andryss.observer.generated.yandexgpt.model.ReasoningOptions;
+
+@Slf4j
+@Disabled("For manual testing only")
+public class ManualYandexGptApiTest extends BaseDbTest {
+
+    private static final String IAM_TOKEN = "mock-token";
+    private static final String FOLDER_ID = "mock-folder";
+
+    private YandexGptApi initApi() {
+        YandexGptApi yandexGptApi = new YandexGptApi();
+        yandexGptApi.getApiClient().setBearerToken(IAM_TOKEN);
+        return yandexGptApi;
+    }
+
+    @Test
+    void testCompletion() {
+        YandexGptApi yandexGptApi = initApi();
+
+        CompletionRequest request = new CompletionRequest()
+                .modelUri("gpt://%s/yandexgpt".formatted(FOLDER_ID))
+                .completionOptions(new CompletionOptions()
+                        .stream(false)
+                        .temperature(0.3)
+                        .maxTokens("200")
+                        .reasoningOptions(new ReasoningOptions()
+                                .mode(ReasoningOptions.ModeEnum.DISABLED)
+                        )
+                )
+                .messages(List.of(
+                        new Message()
+                                .role(MessageRole.SYSTEM)
+                                .text("You are chat bot"),
+                        new Message()
+                                .role(MessageRole.USER)
+                                .text("Hi! How are you?")
+                ));
+
+        CompletionResponse response = yandexGptApi.foundationModelsV1CompletionPost(request);
+
+        log.info("{}", response);
+    }
+
+}
