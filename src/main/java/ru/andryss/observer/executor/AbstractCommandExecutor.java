5048b81014ee2d1b87dd5352714a6067708ebcaf
Andryss <andr.krivosheev@gmail.com>
2025-06-21 18:56:22 +0300

    Add AbstractCommandExecutor

---
+++ src/main/java/ru/andryss/observer/executor/AbstractCommandExecutor.java
@@ -0,0 +1,30 @@
+package ru.andryss.observer.executor;
+
+import java.util.Objects;
+
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public abstract class AbstractCommandExecutor implements UpdateExecutor {
+
+    public abstract CommandInfo getCommandInfo();
+
+    @Override
+    public boolean canProcess(Update update) {
+        return update.hasMessage() &&
+                update.getMessage().hasEntities() &&
+                update.getMessage().getEntities().stream()
+                        .filter(entity -> Objects.equals("bot_command", entity.getType()))
+                        .map(this::extractCommand)
+                        .anyMatch(getCommandInfo().command()::equals);
+    }
+
+    private String extractCommand(MessageEntity entity) {
+        String text = entity.getText();
+        int tagIndex = text.indexOf('@');
+        return tagIndex == -1 ? text : text.substring(0, tagIndex);
+    }
+
+    public record CommandInfo(String command) {
+    }
+}
--- src/main/java/ru/andryss/observer/executor/GetChatIdCommandExecutor.java
+++ src/main/java/ru/andryss/observer/executor/GetChatIdCommandExecutor.java
@@ -1,20 +1,21 @@
 package ru.andryss.observer.executor;
 
-import java.util.Objects;
-
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import ru.andryss.observer.service.KeyStorageService;
 
 @Component
 @RequiredArgsConstructor
-public class GetChatIdCommandExecutor implements UpdateExecutor {
+public class GetChatIdCommandExecutor extends AbstractCommandExecutor {
 
     private final KeyStorageService keyStorageService;
+
+    @Getter
+    private final CommandInfo commandInfo = new CommandInfo("/chatid");
 
     @Override
     public boolean isActive() {
@@ -22,24 +23,8 @@ public class GetChatIdCommandExecutor implements UpdateExecutor {
     }
 
     @Override
-    public boolean canProcess(Update update) {
-        return update.hasMessage() &&
-                update.getMessage().hasEntities() &&
-                update.getMessage().getEntities().stream()
-                        .filter(entity -> Objects.equals("bot_command", entity.getType()))
-                        .map(this::extractCommand)
-                        .anyMatch("chatid"::equals);
-    }
-
-    @Override
     public void process(Update update, AbsSender sender) throws Exception {
         String chatId = update.getMessage().getChatId().toString();
         sender.execute(new SendMessage(chatId, chatId));
-    }
-
-    private String extractCommand(MessageEntity entity) {
-        String text = entity.getText();
-        int tagIndex = text.indexOf('@');
-        return text.substring(1, tagIndex == -1 ? text.length() : tagIndex);
     }
 }
